7dcb067613b637b2a21ec9e36b5800462f917f44
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-08-13 16:30:00 -0400

    Comment out unused Image import in InfoDetail component

--- src/app/components/ui/InfoDetail.tsx
+++ src/app/components/ui/InfoDetail.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { useState } from 'react'
-import Image from "next/image";
+//import Image from "next/image";
 import { Star } from "lucide-react";
 
 function InfoDetail() {
