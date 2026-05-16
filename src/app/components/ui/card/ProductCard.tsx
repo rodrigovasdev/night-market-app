458cb62d9e47820a2d63a0dcea1c6438949e5ba4
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2026-05-16 13:56:37 -0400

    Refactor ProductCard component to accept props for dynamic product data

--- src/app/components/ui/card/ProductCard.tsx
+++ src/app/components/ui/card/ProductCard.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 import Image from "next/image";
 import Link from 'next/link';
 
-function ProductCard() {
+interface ProductCardProps {
+  title: string;
+  shortDescription: string;
+  price: string;
+  imageUrl: string;
+}
+
+function ProductCard({ title, shortDescription, price, imageUrl }: ProductCardProps) {
   return (
     <Link href={'/products/details'}>
       <div className="flex flex-col space-y-2 border-1 bg-white border-gray-200 rounded-lg hover:cursor-pointer hover:shadow-lg hover:border-1 hover:border-gray-900 transition duration-200 ease-in-out">
                   <Image
                     className="rounded-t-lg rounded-l-lg rounded-r-lg rounded-b-none"
-                    src="/assets/ecommerce.jpg"
+                    src={imageUrl}
                     alt="Producto"
                     width = {500}
                     height = {300}
                   />
                   <div className = "flex flex-col px-2 gap-3 py-5">
-                    <span className="font-semibold">Modern Chair</span>
-                    <span className="text-gray-500">Ergonomic and durable.</span>
-                    <span className="text-slate-900 font-bold">$199</span>
+                    <span className="font-semibold">{title}</span>
+                    <span className="text-gray-500">{shortDescription}</span>
+                    <span className="text-slate-900 font-bold">{price}</span>
                     <span className="text-gray-500">Free shipping</span>
                   </div>
         </div>
