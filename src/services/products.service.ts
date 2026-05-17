6b3e3dc6bfdaa2de29216880f9bc7dfc48436794
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2026-05-16 22:20:25 -0400

    Refactor products service to improve product fetching functions

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -10,3 +10,25 @@ export async function getProducts(): Promise<Product[]> {
 
   return response.json();
 }
+
+export async function getLatestProducts(): Promise<Product[]> {
+  const response = await fetch(`http://localhost:3000/api/products/latest`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch products: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+
+export async function getProduct(id: number): Promise<Product> {
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch products: ${response.status}`);
+  }
+
+  return response.json();
+}
+
