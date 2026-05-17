0fc8a1882ef74043d2f2f31c382d867c6e46f2a3
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2026-05-16 22:21:18 -0400

    Refactor ProductInfo component to accept props for dynamic content rendering

--- src/app/components/sections/product/ProductInfo.tsx
+++ src/app/components/sections/product/ProductInfo.tsx
@@ -2,15 +2,21 @@ import Button from '@/app/components/ui/Button';
 import InfoDetail from '@/app/components/ui/InfoDetail';
 import SatisfactionForm from '@/app/components/sections/product/SatisfactionFormulary';
 
-const ProductInfo = () => (
+interface ProductInfoProps {
+  title: string;
+  shortDescription: string;
+  longDescription: string;
+  price: number;
+  specifications?: string;
+}
+const ProductInfo = ({ title, shortDescription, longDescription, price, specifications }: ProductInfoProps) => (
   <div className="flex flex-col items-start w-full pt-5 md:pt-0">
-    <span className="text-2xl font-bold">Yellow-shirt</span>
-    <span className="text-md pt-5">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec varius libero. Nulla facilisi. Cras pellentesque felis ut consectetur blandit. Nam eget augue euismod, gravida nisi ac, interdum neque. Fusce nec tincidunt est. Integer pretium vestibulum risus, sit amet rutrum nisi suscipit quis. Aenean sed justo sed justo facilisis pulvinar.
-    </span>
-    <span className="text-2xl font-bold pt-5 pb-5">$25.00</span>
+    <span className="text-2xl font-bold">{title}</span>
+    <span className="text-md pt-5">{shortDescription}</span>
+    <span className="text-md pt-5">{longDescription}</span>
+    <span className="text-2xl font-bold pt-5 pb-5">${price}</span>
     <Button variant='primary' width='w-1/2 md:w-1/4' content='Add to Cart' />
-    <InfoDetail />
+    <InfoDetail specifications={specifications || ''} />
     <div className="flex w-full justify-center pt-20 block md:hidden">
       <SatisfactionForm />
     </div>
