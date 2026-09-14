07f6ef6156ed6960f36433c9dedb59d1508f553b
rodrigovasdev <rodrigo.vasqueze@gmail.com>
2026-09-14 00:16:34 -0300

    refactor: simplify ProductsShowcase layout by removing unnecessary Container and CardContainer components

--- src/components/home/ProductsShowcase.tsx
+++ src/components/home/ProductsShowcase.tsx
@@ -2,8 +2,6 @@
 
 import ProductCard from "@/components/ui/card/ProductCard";
 import ProductCardSkeleton from "@/components/ui/card/ProductCardSkeleton";
-import Container from "@/components/ui/Container";
-import CardContainer from "@/components/ui/CardContainer";
 import Button from "@/components/ui/Button";
 import { Product } from "@/types/product.types";
 
@@ -27,37 +25,35 @@ export default function ProductsShowcase({
     const visibleProducts = products.slice(0, 4);
 
     return (
-        <Container id={id}>
-            <CardContainer>
-                <div className="flex py-5 pb-15 items-center justify-between">
-                    <h1 className="font-bold text-3xl md:text-4xl ">{title}</h1>
-                    <Button
-                        content={actionLabel}
-                        type="link"
-                        href={actionHref}
-                        width="text-center w-1/2 md:w-1/4 2xl:w-1/8"
-                        variant="secondary"
-                    />
-                </div>
+        <section id={id} className="w-full bg-white px-4 py-20 md:px-40">
+            <div className="flex py-5 pb-15 items-center justify-between">
+                <h1 className="font-bold text-3xl md:text-4xl ">{title}</h1>
+                <Button
+                    content={actionLabel}
+                    type="link"
+                    href={actionHref}
+                    width="text-center w-1/2 md:w-1/4 2xl:w-1/8"
+                    variant="secondary"
+                />
+            </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-3 justify-items-center">
-                    {isLoading
-                        ? Array.from({ length: 4 }).map((_, index) => (
-                              <ProductCardSkeleton key={index} />
-                          ))
-                          : visibleProducts.map((product) => (
-                              <ProductCard
-                                  key={product.id}
-                                  title={product.name}
-                                  shortDescription={product.shortDescription}
-                                  price={product.price}
-                                  imageUrl={product.images[0]?.url || "/assets/ecommerce.jpg"}
-                                  imageUrl2={product.images[1]?.url || "/assets/ecommerce.jpg"}
-                                  id={product.id}
-                              />
-                          ))}
-                </div>
-            </CardContainer>
-        </Container>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-3 justify-items-center">
+                {isLoading
+                    ? Array.from({ length: 4 }).map((_, index) => (
+                          <ProductCardSkeleton key={index} />
+                      ))
+                      : visibleProducts.map((product) => (
+                          <ProductCard
+                              key={product.id}
+                              title={product.name}
+                              shortDescription={product.shortDescription}
+                              price={product.price}
+                              imageUrl={product.images[0]?.url || "/assets/ecommerce.jpg"}
+                              imageUrl2={product.images[1]?.url || "/assets/ecommerce.jpg"}
+                              id={product.id}
+                          />
+                      ))}
+            </div>
+        </section>
     );
 }
